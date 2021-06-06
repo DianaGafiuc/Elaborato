f2ec626113ef7b9e1dda303ac02c7e68ab51e9e9
DianaGafiuc <65336134+DianaGafiuc@users.noreply.github.com>
2021-06-06 23:09:21 +0200

    Delete connessione.php

--- connessione.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";      //utente root = amministratore
-$password = "";          //lasciare vuoto perchè non c'è una password impostata
-$dbname = "poggers";         //nome del nostro database
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-?>
